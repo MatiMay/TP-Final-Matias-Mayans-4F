8e07fb84fa1e743bd21aa0f7fc33e2f883879c29
MAYANS MATÍAS <50926612@est.ort.edu.ar>
2024-06-28 08:57:22 -0300

    Initial commit

---
+++ Assets/ObjetosScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjetosScript : MonoBehaviour
+{
+    public GameObject[] objetos;
+    GameObject nuevobj;
+    int a = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        nuevobj = Instantiate(objetos[0], new Vector3(0, 0, 0), Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) 
+        {
+            Destroy(nuevobj);
+            if (a >= objetos.Length - 1)  a = 0; 
+            else a++;
+            nuevobj = Instantiate(objetos[a], new Vector3(0, 0, 0), Quaternion.identity);
+            Debug.Log(a);
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Destroy(nuevobj);
+            if (a <= 0) a = objetos.Length - 1;
+            else a--;
+            nuevobj = Instantiate(objetos[a], new Vector3(0, 0, 0), Quaternion.identity);
+            Debug.Log(a);
+        }
+    }
+}
